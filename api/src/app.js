11032c3c6e03843697267dec87cb2dc6a1e0d259
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-22 14:11:25 +0100

    lesson 8 initial

---
+++ api/src/app.js
@@ -0,0 +1,15 @@
+
+const Express = require("express");
+const cors = require("cors")
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"]
+}))
+
+
+module.exports = app
