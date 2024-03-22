ba9feedeeca3ce3dc67e4072cb4eb81747339e15
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-22 15:55:23 +0100

    add controller and routes

--- api/src/controllers/author.controller.js
+++ api/src/controllers/author.controller.js
@@ -1,27 +1,82 @@
 const Author = require("../models/author.model");
 
-
 async function createAuthor(req, res) {
-    try {
-        const _author = req.body
-        const author = await Author.create(_author)
-        res.status(201).json(author)
-    } catch (error) {
-        console.error("Error creating author: ", error.message)
-        if (error.message.includes("validation failed")) {   
-            return res.status(400).json({
-                message: error.message
-            })
-        }
-        res.status(500).json({
-            message: "Error createing author: " + error.message
-        })
+  try {
+    const _author = req.body;
+    const author = await Author.create(_author);
+    res.status(201).json(author);
+  } catch (error) {
+    console.error("Error creating author: ", error.message);
+    if (error.message.includes("validation failed")) {
+      return res.status(400).json({
+        message: error.message,
+      });
     }
+    res.status(500).json({
+      message: "Error creating author: " + error.message,
+    });
+  }
 }
 
-//TODO: READ all authors
-//TODO: READ author on id
+async function getAuthors(req, res) {
+  try {
+    const authors = await Author.find();
+    res.json(authors);
+  } catch (error) {
+    console.error("Error getting authors: ", error.message);
+    res.status(500).json({
+      message: "Error getting authors: " + error.message,
+    });
+  }
+}
+
+async function getAuthor(req, res) {
+  const { id } = req.params;
+  try {
+    const author = await Author.findById(id);
+    if (!author) {
+      throw new Error("Cast to ObjectId");
+    }
+    res.json(author);
+  } catch (error) {
+    console.error("Error getting author: ", error.message);
+    if (error.message.includes("Cast to ObjectId")) {
+      return res.status(404).json({
+        message: "Author not found",
+      });
+    }
+    res.status(500).json({
+      message: "Error getting authors: " + error.message,
+    });
+  }
+}
+
+async function updateAuthor(req, res) {
+  try {
+    //TODO: update author
+    res.json("Unimplemented");
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
+
+async function deleteAuthor(req, res) {
+  try {
+    //TODO: delete author
+    res.json("Unimplemented");
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
 
 module.exports = {
-    createAuthor
-}
+  createAuthor,
+  getAuthors,
+  getAuthor,
+  updateAuthor,
+  deleteAuthor,
+};
--- api/src/routes/author.route.js
+++ api/src/routes/author.route.js
@@ -1,9 +1,19 @@
 const Express = require("express");
 
-const { createAuthor } = require("../controllers/author.controller");
+const {
+  createAuthor,
+  getAuthors,
+  getAuthor,
+  updateAuthor,
+  deleteAuthor,
+} = require("../controllers/author.controller");
 
-const authorRouter = Express.Router()
+const authorRouter = Express.Router();
 
 authorRouter.post("/", createAuthor);
+authorRouter.get("/", getAuthors);
+authorRouter.get("/:id", getAuthor);
+authorRouter.put("/:id", updateAuthor);
+authorRouter.delete("/:id", deleteAuthor);
 
-module.exports = authorRouter
+module.exports = authorRouter;
