ec6199061c64a7cc8b5f9ca6e244bb87ec6cf4ee
pinsamt <felix.vallejo99v@gmail.com>
2024-03-25 15:35:50 +0100

    daadda

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("GET request to the homepage");
+  res.send("Library");
 });
 
 app.use(
