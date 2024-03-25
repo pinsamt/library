76875b31acf2bd4150b245aed17d17ec348dbc2c
pinsamt <felix.vallejo99v@gmail.com>
2024-03-25 13:52:40 +0100

    New repoNew repoNew repoNew repoNew repoNew repoNew repoNew repoNew repoNew repo

--- api/src/config/mongoose.js
+++ api/src/config/mongoose.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const databaseURI = process.env.MONGOOSE_LOCAL_URI;
+const databaseURI = process.env.MONGOD_LIVE_URI;
 
 function connectToMongoose() {
-    mongoose.connect(databaseURI)
-        .then(() => {
-            console.log("Connected to DB: ", databaseURI)
-        })
-        .catch((error) => {
-            console.log("Could not connect to DB: ", databaseURI, error)
-        })
+  mongoose
+    .connect(databaseURI)
+    .then(() => {
+      console.log("Connected to DB: ", databaseURI);
+    })
+    .catch((error) => {
+      console.log("Could not connect to DB: ", databaseURI, error);
+    });
 }
 
 module.exports = {
-    connectToMongoose
-}
+  connectToMongoose,
+};
