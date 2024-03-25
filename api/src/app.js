b7d858971c4b01ccc5504cdda8162f03258ac04b
pinsamt <felix.vallejo99v@gmail.com>
2024-03-25 15:42:13 +0100

    Changed order of app.get

--- api/src/app.js
+++ api/src/app.js
@@ -8,17 +8,17 @@ const app = Express();
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Library API",
-  });
-});
-
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
   })
 );
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Library API",
+  });
+});
 
 app.use("/api/v1/authors", authorRouter);
 app.use("/api/v1/books", bookRouter);
